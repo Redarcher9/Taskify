deba61339d0b2cd1c4be2fae3222468d93742a37
Redarcher9 <snehit.86@gmail.com>
2023-10-10 22:29:17 +0530

    Removed ping calls from main file

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"taskify/api/routes"
 	"taskify/bootstrap"
 
@@ -12,12 +11,7 @@ func main() {
 	r := gin.Default()
 	env := bootstrap.NewEnv()
 	db := bootstrap.SetupDB(*env)
-	error := db.Ping()
 	routes.SetupRoutes(*env, db, r)
-	if error != nil {
-		fmt.Println("Db is working")
-	}
-
 	r.Run() // listen and serve on 0.0.0.0:8080
 	db.Close()
 }
