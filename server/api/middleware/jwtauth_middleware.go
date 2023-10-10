1dee9ea639b09dc11f6baa72ddfb376f595b2d05
Redarcher9 <snehit.86@gmail.com>
2023-10-10 22:09:58 +0530

    Added Httponly client cookie support for JWT authentication

--- server/api/middleware/jwtauth_middleware.go
+++ server/api/middleware/jwtauth_middleware.go
@@ -9,7 +9,15 @@ import (
 
 func JwtAuthMiddleware(secret string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		err := utils.ValidateToken(c, token)
+		cookie, err := c.Cookie("Auth_Token")
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"Message": err.Error(),
+			})
+			c.Abort()
+			return
+		}
+		err = utils.ValidateToken(c, cookie)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{
 				"Message": err.Error(),
